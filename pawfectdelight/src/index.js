ff31d2e7ffb15d7dd15769f5ec81e0bb1a385c83
root <fields.matt11@gmail.com>
2018-03-24 11:37:39 -0400

    March 24

--- pawfectdelight/src/index.js
+++ pawfectdelight/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import whoweare from './whoweare'
 import products from './products'
